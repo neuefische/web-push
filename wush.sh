3bab4124100d1e8ded542118344ee4517818f676
Felix Kirchhoff <f.kirchhoff@proton.me>
2024-03-08 14:32:08 +0100

    enable git commands

--- wush.sh
+++ wush.sh
@@ -85,7 +85,7 @@ if [ ! -d ".git" ]; then
     exit 1
   fi
 
-  # git init
+  git init
 fi
 
 repository_name="$cohort_id"-"$session"
@@ -96,9 +96,9 @@ if ! confirm "A new remote repository $(highlightText "$repository_name") will b
   exit 0
 fi
 
-# Commit everything and upload the repository
-# git add . && git commit -m "initial commit"
-# gh repo create -s=. --push --public --remote=origin neuefische-web-demos/"$repository_name"
+Commit everything and upload the repository
+git add . && git commit -m "initial commit"
+gh repo create -s=. --push --public --remote=origin neuefische-web-demos/"$repository_name"
 
 if [ $? -eq 0 ]; then
   echo $(highlightText "Repository created: https://github.com/neuefische-web-demos/$repository_name")
